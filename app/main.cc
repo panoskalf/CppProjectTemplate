8300c80c6ff4cfd769356831c430e56900eded58
Panagiotis Kalfoudis <panagiotis.kalfoudis@siemens.com>
2025-03-11 11:37:00 +0100

    update ubuntu build to troubleshoot
    enable some cmake options
    uncomment lines in main to trigger clang-tidy warnings
    run cmake-format and clang-format

--- app/main.cc
+++ app/main.cc
@@ -27,11 +27,11 @@ int main(int argc, char **argv)
     std::cout << "\n\nUsage Example:\n";
 
     // Compiler Warning and clang tidy error
-    // std::int32_t i = 0;
+    std::int32_t i = 0;
 
     // Adress Sanitizer should see this
-    // char x[10];
-    // x[11] = 1;
+    char x[10];
+    x[11] = 1;
 
     const auto welcome_message =
         fmt::format("Welcome to {} v{}\n", project_name, project_version);
--- cmake/AddGitSubmodule.cmake
+++ cmake/AddGitSubmodule.cmake
@@ -3,13 +3,13 @@ function(add_git_submodule relative_dir)
 
     set(FULL_DIR ${CMAKE_SOURCE_DIR}/${relative_dir})
 
-    if (NOT EXISTS ${FULL_DIR}/CMakeLists.txt)
-        execute_process(COMMAND ${GIT_EXECUTABLE}
-            submodule update --init --recursive -- ${relative_dir}
-            WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
+    if(NOT EXISTS ${FULL_DIR}/CMakeLists.txt)
+        execute_process(
+            COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive --
+                    ${relative_dir} WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
     endif()
 
-    if (EXISTS ${FULL_DIR}/CMakeLists.txt)
+    if(EXISTS ${FULL_DIR}/CMakeLists.txt)
         message("Submodule is CMake Project: ${FULL_DIR}/CMakeLists.txt")
         add_subdirectory(${FULL_DIR})
     else()
